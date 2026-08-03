8ec859c805e425cc588473ba2c4f29fd7dad41e8
Frotrest <125584696+frotrest@users.noreply.github.com>
2026-08-03 23:28:01 +0300

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import DesireSection from './Desire/Desire';
-import Fraud from './Fraud/fraud';
+import Fraud from './Fraud/Fraud';
 import Introduction from './Introduction/Introduction';
 import { ChoiceSection } from './Choice/Choice';
 import PropertyServicesSection from './Property/PropertyServicesSection';
